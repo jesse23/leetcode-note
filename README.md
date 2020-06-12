300fb78a506e465ce3c188c5bb4b9badcb9cb6b7
Cirrus CI <support@cirruslabs.org>
2020-06-12 21:33:36 +0000

    #122

---
+++ README.md
@@ -0,0 +1,5 @@
+# Current Progress
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
+
+# Category - All
+https://leetcode.com/problemset/all/?search=judge
--- test/leetcode_122.test.js
+++ test/leetcode_122.test.js
@@ -2,12 +2,16 @@ const maxProfit = require('../src/leetcode_122');
 
 describe( 'Leetcode 997', () => {
 
-    xit( 'Input: [7,1,5,3,6,4], Output: 7', () => {
+    it( 'Input: [7,1,5,3,6,4], Output: 7', () => {
         expect( maxProfit( [7,1,5,3,6,4] ) ).toEqual( 7 );
     } );
 
-    xit( 'Input: [1,2,3,4,5], Output: 4', () => {
-        expect( maxProfit( [7,1,5,3,6,4] ) ).toEqual( 4 );
+    it( 'Input: [1,2,3,4,5], Output: 4', () => {
+        expect( maxProfit( [1,2,3,4,5] ) ).toEqual( 4 );
+    } );
+
+    it( 'Input: [7,6,4,3,1], Output: 4', () => {
+        expect( maxProfit( [7,6,4,3,1] ) ).toEqual( 0 );
     } );
 
 } );
