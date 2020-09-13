0ac88c65370c337f268896788e861a2749651007
jesse23 <pengwenjia@gmail.com>
2020-09-13 02:35:58 +0000

    hr min complixety

--- src/hr_min_complexity.js
+++ src/hr_min_complexity.js
@@ -1,48 +1,175 @@
 // Hacker Rank - findMinComplexity 
 
-// - what is the peak value?
-// - how many peak values do u have?
-// - the gaps between different gap is un predictable 
-var findMinComplexity = function( complexity, days ) {
+function getWeightArr( complexity ) {
+    const data = complexity.map( ( v, i ) => ( { val: v, idx: i } ) ).sort( ( a, b ) => ( b.val === a.val ? a.idx - b.idx : b.val - a.val ) );
 
-    let sum = 0;
-    startIdx = 0;
-    endIdx = complexity.length - 1;
-    let maxVal = Math.max( ...complexity );
+    const maxPoint = data[ 0 ];
 
-    // extreme case
-    if( days === 0 ) {
-        return 0;
+    const weightArr = [];
+
+    for( let i = 1, idxArr = [ maxPoint.idx ]; i < complexity.length; i++ ) {
+        const currPoint = data[ i ];
+
+        // console.log( idxArr );
+
+        // find idx
+        const rIdxOfIdx = idxArr.findIndex( e => e > currPoint.idx );
+        if( rIdxOfIdx === -1 ) {
+            // currPoint.idx at the end
+            const lIdxOfIdx = idxArr.length - 1;
+            const step = currPoint.idx - idxArr[ lIdxOfIdx ];
+            let value = 0;
+            for( let j = idxArr[ lIdxOfIdx ] + 1; j < currPoint.idx + 1; j++ ) {
+                value += complexity[ j ];
+            }
+            const weight = value / step;
+
+            weightArr.push( {
+                weight: weight,
+                sIdx: idxArr[ lIdxOfIdx ] + 1,
+                eIdx: currPoint.idx + 1
+            } )
+
+            idxArr.push( currPoint.idx );
+
+        } else if( rIdxOfIdx === 0 ) {
+            // currPoint.idx at the beginning
+            const step = idxArr[ rIdxOfIdx ] - currPoint.idx;
+            let value = 0;
+            for( let j = currPoint.idx; j < idxArr[ rIdxOfIdx ]; j++ ) {
+                value += complexity[ j ];
+            }
+            const weight = value / step;
+
+            weightArr.push( {
+                weight: weight,
+                sIdx: currPoint.idx,
+                eIdx: idxArr[ rIdxOfIdx ]
+            } )
+
+            idxArr.unshift( currPoint.idx );
+
+        } else {
+            // currPoint.idx in the middle
+            const rStep = idxArr[ rIdxOfIdx ] - currPoint.idx;
+            let rValue = 0;
+            for( let j = currPoint.idx; j < idxArr[ rIdxOfIdx ]; j++ ) {
+                rValue += complexity[ j ];
+            }
+
+            const lWeightObj = {
+                weight: rValue / rStep,
+                sIdx: currPoint.idx,
+                eIdx: idxArr[ rIdxOfIdx ]
+            }
+
+            weightArr.push( lWeightObj );
+
+            const lIdxOfIdx = rIdxOfIdx - 1;
+            if( lWeightObj.sIdx !== idxArr[ lIdxOfIdx ] + 1 || lWeightObj.eIdx !== currPoint.idx + 1 ) {
+                const lStep = currPoint.idx - idxArr[ lIdxOfIdx ];
+                let lValue = 0;
+                for( let j = idxArr[ lIdxOfIdx ] + 1; j < currPoint.idx + 1; j++ ) {
+                    lValue += complexity[ j ];
+                }
+
+                weightArr.push( {
+                    weight: lValue / lStep,
+                    sIdx: idxArr[ lIdxOfIdx ] + 1,
+                    eIdx: currPoint.idx + 1
+                } )
+            }
+
+            idxArr.splice( lIdxOfIdx + 1, 0, currPoint.idx );
+        }
     }
+
+    weightArr.sort( ( a, b ) => b.weight - a.weight );
+
+    return weightArr;
+}
+
+function getMinVal( complexity, weightArr, amount, dp = {}, preLoadWeightIdxArr = [] ) {
+
+    // return directly if value exist
+    if( dp[ JSON.stringify( preLoadWeightIdxArr ) ] ) {
+        return dp[ JSON.stringify( preLoadWeightIdxArr ) ];
+    }
+
+    const removeMap = {};
+
+    // load preLoadWeightIdxArr
+    let i = 0;
+    for( let j = 0; j < preLoadWeightIdxArr.length; j++ ) {
+        const curr = weightArr[ preLoadWeightIdxArr[ j ] ];
+        for( let k = curr.sIdx; k < curr.eIdx; k++ ) {
+            if( !removeMap[ k ] ) {
+                removeMap[ k ] = true;
+                i++;
+            }
+        }
+    }
+
+    // it might be greater than amount...set it to 'not option'
+    if( i > amount ) {
+        return dp[ 'MAX' ];
+    }
+
+    const pendingWeightArr = [];
+    for( let j = 0; i < amount && j < weightArr.length; j++ ) {
+        const curr = weightArr[ j ];
+
+        const localRemovedMap = {};
+        let d = 0;
+
+        for( let k = curr.sIdx; k < curr.eIdx; k++ ) {
+            if( !removeMap[ k ] ) {
+                localRemovedMap[ k ] = true;
+                d++;
+            }
+        }
+
+        if( d > amount ) {
+            continue;
+        } else if( d + i > amount ) {
+            // do something
+            pendingWeightArr.push( j );
+            continue;
+        } else {
+            Object.assign( removeMap, localRemovedMap );
+            i += d;
+        }
+    }
+
+    // console.log( weightArr );
+
+    // i !== amount case is impossible, set it to max for now
+    const val = ( i === amount ) ? complexity.reduce( ( sum, v, i ) => sum + ( removeMap[ i ] ? 0 : v ), 0 ) : dp[ 'MAX' ];
+
+    // cache to dp
+    dp[ JSON.stringify( preLoadWeightIdxArr ) ] = val;
+
+    return pendingWeightArr.reduce( ( sum, v ) => {
+        const newVal = getMinVal( complexity, weightArr, amount, dp, [ v, ...preLoadWeightIdxArr ] );
+        return Math.min( sum, newVal );
+    }, val );
+}
+
+var findMinComplexity = function( complexity, days ) {
+    const dp = {};
+
+    const maxVal = Math.max( ...complexity );
+
+    dp[ 'MAX' ] = complexity.reduce( ( sum, v ) => sum + v, 0 );
+
     if( days === 1 ) {
         return maxVal;
     } else if( days === 2 ) {
         return maxVal + Math.min( complexity[ 0 ], complexity[ complexity.length - 1 ] );
-    } else {
-        // max is given
-        sum += maxVal;
-        for( let i = 1; i < days; i++ ) {
-            if( complexity[ startIdx ] < complexity[ endIdx ] ) {
-                if( complexity[ endIdx ] === maxVal ) {
-                    sum += findMinComplexity( complexity.slice( startIdx, endIdx ), days - i );
-                    break;
-                } else {
-                    sum += complexity[ startIdx ];
-                    startIdx++;
-                }
-            } else {
-                if( complexity[ startIdx ] === maxVal ) {
-                    sum += findMinComplexity( complexity.slice( startIdx + 1, endIdx + 1 ), days - i );
-                    break;
-                } else {
-                    sum += complexity[ endIdx ];
-                    endIdx--;
-                }
-            }
-        }
-
-        return sum;
+    } else if( days === complexity.length ) {
+        return dp[ 'MAX' ];
     }
+    return getMinVal( complexity, getWeightArr( complexity ), complexity.length - days, dp );
 };
 
 module.exports = findMinComplexity;
--- test/hr_min_complexity.test.js
+++ test/hr_min_complexity.test.js
@@ -2,6 +2,42 @@ const findMinComplexity = require( '../src/hr_min_complexity' );
 
 describe( 'Test cases for hr_min_complexity', () => {
 
+    it( 'Test 1 day use case in sorted complexity', () => {
+        expect( findMinComplexity( [ 1, 2, 3, 4, 5 ], 1 ) ).toEqual( 5 );
+    } );
+
+    it( 'Test 2 day use case in all single step weight', () => {
+        expect( findMinComplexity( [ 5, 4, 3, 2, 1 ], 2 ) ).toEqual( 6 );
+    } );
+
+    it( 'Test complexity.length === days', () => {
+        expect( findMinComplexity( [ 5, 4, 3, 2, 1 ], 5 ) ).toEqual( 15 );
+    } );
+
+    it( 'Test 1 day use case in unsorted complexity', () => {
+        expect( findMinComplexity( [ 2, 4, 3, 5, 1 ], 1 ) ).toEqual( 5 );
+    } );
+
+    it( 'Test 2 day use case in unsorted complexity', () => {
+        expect( findMinComplexity( [ 2, 4, 3, 5, 1 ], 2 ) ).toEqual( 6 );
+    } );
+
+    it( 'Test 3 day use case in unsorted complexity', () => {
+        expect( findMinComplexity( [ 2, 4, 3, 5, 1 ], 3 ) ).toEqual( 8 );
+    } );
+
+    it( 'Test 3 day use case in unsorted complexity', () => {
+        expect( findMinComplexity( [ 2, 4, 3, 5, 1 ], 4 ) ).toEqual( 12 );
+    } );
+
+    it( 'test case days = complexity.length', () => {
+        expect( findMinComplexity( [ 2, 5, 1, 4, 3 ], 5 ) ).toEqual( 15 );
+    } );
+
+    it( 'test case days = complexity.length', () => {
+        expect( findMinComplexity( [ 5, 4, 3, 2, 1 ], 4 ) ).toEqual( 11 );
+    } );
+
     it( 'test case days = 1', () => {
         expect( findMinComplexity( [ 1, 5, 3, 2, 4 ], 1 ) ).toEqual( 5 );
     } );
@@ -19,7 +55,11 @@ describe( 'Test cases for hr_min_complexity', () => {
     } );
 
     it( 'test case days = 3', () => {
-        expect( findMinComplexity( [ 4, 2, 1, 5, 3 ], 3 ) ).toEqual( 11 );
+        expect( findMinComplexity( [ 4, 2, 1, 5, 3 ], 3 ) ).toEqual( 10 );
+    } );
+
+    it( 'test case days = 3', () => {
+        expect( findMinComplexity( [ 4, 2, 1, 5, 3 ], 2 ) ).toEqual( 8 );
     } );
 
     it( 'test case days = 3', () => {
@@ -30,19 +70,18 @@ describe( 'Test cases for hr_min_complexity', () => {
         expect( findMinComplexity( [ 2, 3, 1, 4 ], 3 ) ).toEqual( 8 );
     } );
 
-    xit( 'test case days = 3', () => {
-        expect( findMinComplexity( [ 4, 1, 5, 2, 2 ], 3 ) ).toEqual( 9 );
+    it( 'test case days = 3', () => {
+        expect( findMinComplexity( [ 5, 1, 6, 4, 3, 2 ], 4 ) ).toEqual( 14 );
+    } );
+    it( 'test case days = 3', () => {
+        expect( findMinComplexity( [ 5, 1, 6, 4, 3, 2 ], 3 ) ).toEqual( 11 );
     } );
 
-    xit( 'test case days = 3', () => {
+    it( 'test case days = 3', () => {
         expect( findMinComplexity( [ 3, 6, 2, 1, 7, 4, 5 ], 3 ) ).toEqual( 14 );
     } );
 
-    xit( 'test case days = 4', () => {
+    it( 'test case days = 4', () => {
         expect( findMinComplexity( [ 3, 6, 2, 1, 7, 4, 5 ], 4 ) ).toEqual( 16 );
-    } );
-
-    xit( 'test case days = complexity.length', () => {
-        expect( findMinComplexity( [ 2, 5, 1, 4, 3 ], 5 ) ).toEqual( 15 );
     } );
 } );
